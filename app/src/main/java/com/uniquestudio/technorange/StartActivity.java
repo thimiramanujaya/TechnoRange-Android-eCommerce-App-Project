63a703543a237fc2253ebaaf61601d068889f351
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-11-17 23:44:17 +0530

    implemented complete StartActivity.java

---
+++ app/src/main/java/com/uniquestudio/technorange/StartActivity.java
@@ -0,0 +1,74 @@
+package com.uniquestudio.technorange;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ViewPager viewPagerSlide;
+    ViewPageAdapter viewPageAdapter;
+    TabLayout tabLayout;
+    ImageButton nextBtn;
+    int position = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // set activity to fullscreen *Note- Insert snippet before setting Activity layout xml
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_start);
+
+        // hide action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        List<sliderLayoutContent> layoutContent = new ArrayList<>();
+        // Add Contents to viewPagersSlides
+        layoutContent.add(new sliderLayoutContent(R.string.onboard_heading_one, R.string.onboard_descript_one, R.drawable.onboarding_img_1));
+        layoutContent.add(new sliderLayoutContent(R.string.onboard_heading_two, R.string.onboard_descript_two, R.drawable.onboarding_img_2));
+        layoutContent.add(new sliderLayoutContent(R.string.onboard_heading_three, R.string.onboard_descript_three, R.drawable.onboarding_img_3));
+
+        //setup viewpager
+        viewPagerSlide = findViewById(R.id.view_pager_slide);
+        viewPageAdapter = new ViewPageAdapter(this, layoutContent);
+        viewPagerSlide.setAdapter(viewPageAdapter);
+
+
+        tabLayout = findViewById(R.id.dot_layout);
+        tabLayout.setupWithViewPager(viewPagerSlide);
+
+        nextBtn = findViewById(R.id.next_btn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position = viewPagerSlide.getCurrentItem();
+                if(position < layoutContent.size()) {
+                    position++;
+                    viewPagerSlide.setCurrentItem(position);
+                }
+                if(position == layoutContent.size()) {  // when reach to the last page
+                    loadLastPage();
+                }
+            }
+        });
+
+    }
+
+    private void loadLastPage() {
+    }
+}
