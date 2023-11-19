3e6ea31f6c983d2b0eb046daea0674380218052d
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-11-19 18:25:46 +0530

    implemented MainActivity button functions

---
+++ app/src/main/java/com/uniquestudio/technorange/CustomerSignup.java
@@ -0,0 +1,14 @@
+package com.uniquestudio.technorange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_signup);
+    }
+}
--- app/src/main/java/com/uniquestudio/technorange/MainActivity.java
+++ app/src/main/java/com/uniquestudio/technorange/MainActivity.java
@@ -2,9 +2,14 @@ package com.uniquestudio.technorange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button joinNowBtn, loginBtn, sellerSignupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        joinNowBtn = findViewById(R.id.join_now_btn);
+        loginBtn = findViewById(R.id.login_btn);
+        sellerSignupBtn = findViewById(R.id.seller_signup_btn);
+
+        joinNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent customerSignUp = new Intent(getApplicationContext(), CustomerSignup.class);
+                startActivity(customerSignUp);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logInActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(logInActivity);
+            }
+        });
+
+        sellerSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sellerSignUp = new Intent(getApplicationContext(), SellerSignup.class);
+                startActivity(sellerSignUp);
+            }
+        });
     }
 }
