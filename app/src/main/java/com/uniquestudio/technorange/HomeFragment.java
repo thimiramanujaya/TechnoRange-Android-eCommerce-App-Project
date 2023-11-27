2f25b6b22c87187e2d9768c88398bb69e9af5a80
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-11-27 23:05:43 +0530

    implemented HomeFragment.java functions

--- app/src/main/java/com/uniquestudio/technorange/HomeFragment.java
+++ app/src/main/java/com/uniquestudio/technorange/HomeFragment.java
@@ -1,64 +1,76 @@
 package com.uniquestudio.technorange;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView greetingTxt, usernameTxt;
+    private RecyclerView.Adapter bannerAdapter;
+    private RecyclerView bannerRecylV;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // return inflater.inflate(R.layout.fragment_home, container, false);  default line
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        greetingTxt = (TextView) view.findViewById(R.id.greeting_txt);
+        usernameTxt = (TextView) view.findViewById(R.id.username_txt);
+        bannerRecylV = (RecyclerView) view.findViewById(R.id.banner_recylv);
+
+        setupBannerRecylV();
+
+        usernameTxt.setText(CurrentUser.currentOnlineUser.getName());
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+        String greeting = null;
+        if(hour < 12) {
+            greeting = "Good Morning!";
+        }
+        else if (hour < 15) {
+            greeting = "Good Afternoon!";
+        }
+        else if (hour < 19) {
+            greeting = "Good Evening!";
+        }
+        else {
+            greeting = "Good Night!";
+        }
+        greetingTxt.setText(greeting);
+
+        return view;
+
+    }
+
+    private void setupBannerRecylV() {
+        LinearLayoutManager linearLytManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        bannerRecylV.setLayoutManager(linearLytManager);
+
+        ArrayList<BannerContent> banner = new ArrayList<>();
+        banner.add(new BannerContent(R.string.banner_heading_one, R.string.banner_descript_one, "", R.drawable.banner_img_1));
+        banner.add(new BannerContent(R.string.banner_heading_two, R.string.banner_descript_two, "", R.drawable.banner_img_2));
+        banner.add(new BannerContent(R.string.banner_heading_three, R.string.banner_descript_three, "", R.drawable.banner_img_3));
+
+        bannerAdapter = new BannerRecylVAdapter(banner);
+        bannerRecylV.setAdapter(bannerAdapter);
     }
 }
